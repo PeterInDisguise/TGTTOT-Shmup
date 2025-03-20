78a6739fe618f3c3bba7861ba32c742a87b7e963
AartvanZee <aartvanzee@gmail.com>
2025-03-20 11:33:30 +0100

    added shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed = 10f;
+
+    private float destroyTimer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destroyTimer = destroyTimer + Time.deltaTime;
+        if (destroyTimer >= 3.5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
