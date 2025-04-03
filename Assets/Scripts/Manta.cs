0f7a023bbbcb7e4ec8437e0ab4a2140ced6d1a8e
AartvanZee <aartvanzee@gmail.com>
2025-04-03 11:48:33 +0200

    call me a manta ray because I'm FINtastic at dad jokes
    started building mantaray

---
+++ Assets/Scripts/Manta.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Manta : enemy
+{
+    private Fishy fish; //this line is not necessary, but I liked it so much that it can stay <3
+    private Rigidbody rb;
+    [SerializeField] private Transform playerPosition;
+
+    private float shootTimer = 0f;
+    [SerializeField] private GameObject electricball;
+    [SerializeField] private Transform electricPoint;
+
+    [SerializeField] private float mantaSpeed = 20f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.linearVelocity = -transform.right * mantaSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, playerPosition.position.y, playerPosition.position.z);
+
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+    }
+}
