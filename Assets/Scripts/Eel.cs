97cba71e701d6c1c5f8cf3f78d43b299653fd05d
AartvanZee <aartvanzee@gmail.com>
2025-04-09 11:34:38 +0200

    added some stuff to eel
    I'm eely sorry for all the bad puns

--- Assets/Scripts/Eel.cs
+++ Assets/Scripts/Eel.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Eel : MonoBehaviour
 {
+    private bool inRange = false;
     [SerializeField] private float spawnTimer = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +13,14 @@ public class Eel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (inRange)
+        {
+            spawnTimer = spawnTimer + Time.deltaTime;
+            if (spawnTimer >= 10f)
+            {
+
+            }
+        }
         
     }
 }
